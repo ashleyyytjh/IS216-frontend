bdabaff9b287407466f77314c51d9f80c0326524
ashley <ashleyyytjh@gmail.com>
2025-09-05 16:33:02 +0800

    build: adding shadcn dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+      <div className="flex items-center justify-center h-dvh bg-background">
+        <div className="text-center space-y-6">
+          <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">Welcome to XXXX</h1>
+          <p className="text-lg text-muted-foreground max-w-[600px] mx-auto">
+            Get started by logging in to your account and explore our market place.
+          </p>
+          <Button color={'primary'}  >
+              Login
+          </Button>
+        </div>
+    </div>
+       
+    )
 }
 
 export default App

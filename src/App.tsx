aa6f4b326921b452ea5bc4801f4956fa7d864c5b
Darren322 <xiia0_darren@hotmail.com>
2025-09-08 17:33:08 +0800

    added store product list

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css'
 import Home from './pages/Home'
 import Layout from './pages/Layout'
 import LoginPage from './pages/Login'
+import Explore from './components/ui/ProductList'
 import {Routes, Route,Link} from 'react-router-dom'
 
 function App() {
@@ -12,9 +13,10 @@ function App() {
     return (
       <>
         <Routes>
-            <Route path = '/'  element={<Layout/>}> 
+               <Route path = '/'  element={<Layout/>}> 
               <Route path="/" element={<Home/>}/>
               <Route path="/login" element={<LoginPage/>}/>
+              <Route path="/explore" element={<Explore/>}/>
             </Route>
         </Routes>
       </>
---
+++ src/pages/Explore.tsx
@@ -0,0 +1,14 @@
+import ProductList from "../components/ui/ProductList";
+
+const Explore = () => {
+  return (
+    <>
+        <div >
+             <ProductList/>
+         </div>
+
+    </>
+  )
+}
+
+export default Explore
